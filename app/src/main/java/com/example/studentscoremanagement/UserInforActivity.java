199b1601d700ded8ebcac4b6a17198afddd13f63
Hai Nam <hnam19052000@gmail.com>
2022-04-09 11:05:47 +0700

    done

---
+++ app/src/main/java/com/example/studentscoremanagement/UserInforActivity.java
@@ -0,0 +1,14 @@
+package com.example.studentscoremanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserInforActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_infor);
+    }
+}
