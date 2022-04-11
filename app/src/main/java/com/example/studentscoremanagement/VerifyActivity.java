e443833cda1371761dcf7b99310e29e70ea45f60
truong-lv <truonglv.2609@gmail.com>
2022-04-11 16:28:21 +0700

    complete Verify SMS form

---
+++ app/src/main/java/com/example/studentscoremanagement/VerifyActivity.java
@@ -0,0 +1,56 @@
+package com.example.studentscoremanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class VerifyActivity extends AppCompatActivity {
+
+    TextView tvMess;
+    EditText txtCode;
+    Button btnComfirm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify);
+
+        setControl();
+        setEvent();
+    }
+
+    private void setEvent() {
+        Intent getDaTa=getIntent();
+        String code=getDaTa.getExtras().getString(LoginActivity.VERIFY_CODE);
+        Bundle bundle=getDaTa.getBundleExtra(LoginActivity.USER_INFOR);
+        String sdt=bundle.getString(DBHelper.COL_TAIKHOAN_SDT);
+        tvMess.setText("Nhập mã xác minh đã được gửi tới SĐT của bạn "+sdt.substring(0,2)+"******"+sdt.substring(sdt.length()-2));
+        btnComfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputCode=txtCode.getText().toString();
+
+                //kiểm tra người dùng nhập mã xác nhận có đúng ko
+                if(inputCode.equals(code)){
+                    Intent intent=new Intent(VerifyActivity.this,ChooseClassActivity.class);
+                    intent.putExtra(LoginActivity.USER_INFOR,bundle);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(VerifyActivity.this, "Mã xác thực không hợp lệ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void setControl() {
+        tvMess=findViewById(R.id.tvSDT);
+        txtCode=findViewById(R.id.txtMaXacThuc);
+        btnComfirm=findViewById(R.id.btnConfirmVerify);
+    }
+}
