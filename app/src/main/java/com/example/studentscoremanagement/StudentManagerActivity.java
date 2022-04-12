edb9dbd536dee0cace62a719c8fbeb8172b9a9f5
Kevin <hiimvyxinh@gmail.com>
2022-04-12 13:17:33 +0700

    Fix bug <<

--- app/src/main/java/com/example/studentscoremanagement/StudentManagerActivity.java
+++ app/src/main/java/com/example/studentscoremanagement/StudentManagerActivity.java
@@ -1,4 +1,14 @@
 package com.example.studentscoremanagement;
 
-public class StudentManagerActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StudentManagerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_manager);
+    }
 }
